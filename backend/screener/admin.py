5331237346f11ffcb0cdd48b71850d383a68c625
David Leong <david0leong@gmail.com>
2020-07-14 09:01:00 -0700

    Add models to admin

--- backend/screener/admin.py
+++ backend/screener/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from .models import Assessment, Domain, Section, Question, Answer
+
 # Register your models here.
+
+
+@admin.register(Assessment)
+class AssessmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Domain)
+class DomainAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
